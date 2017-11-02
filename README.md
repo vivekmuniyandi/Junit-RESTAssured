7cab2fff49513d6aaa1278534d28a853e98a6684
Vivek Muniyandi <vivek.siddharthan@gmail.com>
2017-11-01 20:55:18 -0700

    Added an execute task and modified the REST end point file

--- README.md
+++ README.md
@@ -4,20 +4,24 @@ A sample project which you can just download and get started with Junit and REST
 ## How to Setup?
 Download the project or do a clone of the project. You will have Junit and RESTAssured dependencies already included in the gradle file and you can simply start writing your Junit and RESTAssured tests right away. You can just go to src/test/java and add your Junit and RESTAssured test files in there.
 
-## Project Structure
-
 
 ## How to run the tests?
-Go to the root of the project and simply run
+The current tests present in the src/test/java directory require a sample REST end point returning a json node. So let us start the server by executing
+
+```
+./gradlew -PmainClass=com.example.sample.SampleRESTEndPoint execute
+```
+
+from the root directory of the project. To run the tests, execute
 
 ``` 
 ./gradlew test
 ```
-
-If it is Windows, use the gradlew.bat file. 
 
 This should trigger the job which would take care of compiling the files and running all the tests under src/test/java folder. If you want to run a single test(for example : SampleProgramJunitTest), you can run it using the following command:
 
 ```
 ./gradlew -Dtest.single=SampleProgramJunitTest test
 ```
+
+If the Operating system is Windows, use the gradlew.bat file.
--- src/main/java/com/example/sample/SampleRESTEndPoint.java
+++ src/main/java/com/example/sample/SampleRESTEndPoint.java
@@ -1,15 +1,15 @@
 package com.example.sample;
 
 import org.restlet.Component;
+import org.restlet.data.MediaType;
 import org.restlet.data.Protocol;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-
-public class SimpleREST extends ServerResource {
+public class SampleRESTEndPoint extends ServerResource {
 
   public static void main(String args[]) {
     // Create a new Restlet component and add a HTTP server connector to it
@@ -17,7 +17,7 @@ public class SimpleREST extends ServerResource {
     component.getServers().add(Protocol.HTTP, 8184);
 
     // Then attach it to the local host
-    component.getDefaultHost().attach("/sample", SimpleREST.class);
+    component.getDefaultHost().attach("/sample", SampleRESTEndPoint.class);
 
     // Now, let's start the component!
     // Note that the HTTP server connector is also automatically started.
@@ -29,14 +29,8 @@ public class SimpleREST extends ServerResource {
     }
 }
 
-  @Get("json")
-  public JsonNode retrieve() {
-    ObjectMapper mapper = new ObjectMapper();
-    try {
-      return mapper.readTree("{\"result\":\"success\"}");
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return null;
+  @Get
+  public Representation retrieve() {
+    return new StringRepresentation("{\"result\":\"success\"}", MediaType.APPLICATION_JSON);
   }
 }
